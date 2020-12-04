8fa00b6a9da7c4caf811c4936f0908081b6c1541
stephanie30170 <73656440+stephanie30170@users.noreply.github.com>
2020-12-04 14:00:01 +0100

    Update back-office.php

--- back-office.php
+++ back-office.php
@@ -5,13 +5,12 @@ include_once("session-init.php");
 if (isset($_SESSION["user"]) AND is_array($_SESSION["user"])) {
 
 // yes, alors bienvenue
-include("back-office--template.php"); 
+  include("back-office--template.php"); 
 } 
 
 else { // bye
-header("Location: index.html#mauvaise-session"); exit; // exit après une redirection
+  header("Location: index.html#mauvaise-session"); 
+  exit; // exit après une redirection
 }
-
-
 
 ?>
